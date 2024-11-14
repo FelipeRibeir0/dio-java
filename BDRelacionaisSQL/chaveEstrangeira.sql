4a1dd12d296edf23f3ccd66b8fb6dce12ba69054
FelipeRibeir0 <fecribeiro2003@gmail.com>
2024-11-14 18:34:21 -0300

    fix: type to SQL file || add: chaves estrangeiras

---
+++ BDRelacionaisSQL/chaveEstrangeira.sql
@@ -0,0 +1,14 @@
+/*
+ALTER TABLE reservas
+ADD CONSTRAINT fk_reservas_usuarios
+FOREIGN KEY (id_usuario) REFERENCES usuarios (id);
+
+ALTER TABLE reservas
+ADD CONSTRAINT fk_reservas_destinos
+FOREIGN KEY (id_destino) REFERENCES usuarios (id);
+
+ALTER TABLE reservas
+ADD CONSTRAINT fk_usuarios
+FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
+ON DELETE CASCADE
+*/
