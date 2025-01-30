9334df56f70e16fe75b255d5f9169e4bd14af45b
FelipeRibeir0 <fecribeiro2003@gmail.com>
2025-01-30 18:43:13 -0300

    refactor: Aprendendo sobre Records

--- JavaAbstracao/src/main/java/com/JavaAbstracao/Main.java
+++ JavaAbstracao/src/main/java/com/JavaAbstracao/Main.java
@@ -2,11 +2,8 @@ package com.JavaAbstracao;
 
 public class Main {
     public static void main(String[] args) {
-        var male = new Person("Peterson");
-        male.setAge(12);
-        var female = new Person("Jane");
-        female.setAge(22);
-        System.out.println("Male name: "+ male.getName() + " Age: "+ male.getAge());
-        System.out.println("Female name: "+ female.getName() + " Age: "+ female.getAge());
+        var person = new Person("João", 12);
+        System.out.println(person);
+        System.out.println(person.name());
     }
 }
--- JavaAbstracao/src/main/java/com/JavaAbstracao/Person.java
+++ JavaAbstracao/src/main/java/com/JavaAbstracao/Person.java
@@ -1,32 +1,15 @@
 package com.JavaAbstracao;
 
-import java.time.OffsetDateTime;
+public record Person(String name, int age) {
 
-public class Person {
-    private final String name;
-    private int age;
+    public Person {}
 
-    private int lasYearAgeInc = OffsetDateTime.now().getYear();
-
-    public String getName() {
-        return name;
+    public Person(String name){
+        this(name,1);
     }
 
-    public Person(String name) {
-        this.name = name;
+    public String getInfo(){
+        return "Name: " + name + "\nAge: " + age;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void incAge(){
-        if(this.lasYearAgeInc >= OffsetDateTime.now().getYear()) return;
-        this.age +=1;
-        this.lasYearAgeInc = OffsetDateTime.now().getYear();
-    }
 }
